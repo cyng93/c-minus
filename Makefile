625f1f59f2a2ae8d51429301d78650b780745327
Kok Suan Lim <lks@gidcs.net>
2017-03-18 21:18:27 +0800

    add testcases

--- Makefile
+++ Makefile
@@ -5,8 +5,10 @@ CFLAGS = -lfl
 
 LEX = 0556513
 OUT = scanner
+TEST = helloworld all_pos integer_neg scientific_neg
 
 all: $(OUT)
+test: $(TEST)
 
 %.yy.c: %.l 
 	$(LL) -o $@ $<
@@ -16,3 +18,8 @@ $(OUT): $(LEX).yy.c
 
 clean:
 	rm -f *~ *.yy.c $(OUT)
+
+$(TEST):
+	./scanner testcases/$@.c > test.output
+	diff test.output testcases/$@.gold || true
+	rm -f test.output
---
+++ testcases/all_pos.c
@@ -0,0 +1,148 @@
+/* 1 - Lexical Defination */
+// 1.1 delimeters
+,
+;
+(
+)
+[
+]
+{
+}
+
+// 1.2 arithmetic, relational, logical operators
++
+-
+*
+/
+%
+<=
+<
+=!
+>=
+>
+==
+&&
+||
+!
+
+// 1.3 keyword
+while
+do
+if
+else
+true
+false
+for
+int
+print
+const
+read
+boolean
+bool
+void
+float
+double
+string
+continue
+break
+return
+
+// 1.4 identifier
+ident
+Ident
+IDENT
+_ident
+__indent
+__3ident
+ident3
+
+// 1.5 integer constant
+123
+45
+6
+0
+
+// 1.6 floating point constant
+0.0
+02.03
+123.45
+
+// 1.7 scientific notation
+1.23E4
+1.23E+4
+01.23E-4
+123E04
+
+// 1.8 string constant
+","
+";"
+"("
+")"
+"["
+"]"
+"{"
+"}"
+"+"
+"-"
+"*"
+"/"
+"%"
+"=<"
+"<"
+"=!"
+"=>"
+">"
+"=="
+"&&"
+"||"
+"!"
+"while"
+"do"
+"if"
+"else"
+"true"
+"false"
+"for"
+"int"
+"print"
+"const"
+"read"
+"boolean"
+"bool"
+"void"
+"float"
+"double"
+"string"
+"continue"
+"break"
+"return"
+"ident"
+"Ident"
+"IDENT"
+"_ident"
+"__indent"
+"__3ident"
+"3ident"
+"ident3"
+"ident-3"
+"-indent3"
+"hello world"
+"\"hello world\""
+"hello world\\\\"
+"#pragma source on"
+"#pragma source off"
+"#pragma token on"
+"#pragma token off"
+"#pragma statistic on"
+"#pragma statistic off"
+"//cpp-style comment in string"
+"//c-sytle comment in string"
+
+// 1.9 Comments - funny but we've already use them above ...
+// C++ syntax line comment
+/* C syntax block comment */
+/*
+ * block comment wrap
+ * across
+ * lines.
+ */
